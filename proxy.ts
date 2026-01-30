1929e13d0e5f6245e0efee6ba5ea2ae76611060f
Tyler Varzeas <tyler.varzeas@gmail.com>
2026-01-29 22:59:02 -0800

    Add route protection to dashboard

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { isAuthenticated, redirectToSignIn } = await auth();
+
+  if (!isAuthenticated && isProtectedRoute(req)) {
+    console.log("Access Denied. User not logged in.");
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
-    matcher: [
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        '/(api|trpc)(.*)',
-    ]
-}
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
