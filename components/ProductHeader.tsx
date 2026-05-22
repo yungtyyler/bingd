b8a4530679226a139c3533e4716b5abb877c9f5c
Tyler <tyler.varzeas@gmail.com>
2026-05-22 16:27:45 -0700

    Remove unnecessary import in header

--- components/ProductHeader.tsx
+++ components/ProductHeader.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { UserButton } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
